41702eb4041509101c320dfdd6f448ce675218b3
Esraa Ahmed <esraa.ahmed37722@com>
2019-04-27 19:25:38 +0200

    ESRAA UPDATE

---
+++ app/singlaton.php
@@ -0,0 +1,28 @@
+<?php
+class MySingleton
+{
+    public static function getInstance()
+    {
+        static $instance;
+
+        if (null === $instance) {
+            $instance = new self();
+        }
+
+        return $instance;
+    }
+
+    // prevent creating multiple instances due to "private" constructor
+    private function __construct(){}
+
+    // prevent the instance from being cloned
+    private function __clone(){}
+
+    // prevent from being unserialized
+    private function __wakeup(){}
+}
+  
+
+  //Usage:
+
+//$instance = MySingleton::getInstance();
